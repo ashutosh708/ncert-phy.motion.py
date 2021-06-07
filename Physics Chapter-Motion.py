78b96977a2992870afe2d81a40229d68612bfc8d
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 10:08:24 +0530

    Add files via upload

---
+++ Physics Chapter-Motion.py
@@ -0,0 +1,253 @@
+print("welcome to physics chapter 'MOTION'")
+print("NOTE 1:- DO NOT WRITE ANY TYPE SIGNS e.g.-QUESTION MARK/FULL STOP etc.")
+print("NOTE 2:-WRITE QUESTION IN THE FORM: e.g-what is rest")
+print("")
+print("")
+b=True
+
+while b==True:
+    
+    a=input("enter your question related to this chapter at class 9 level").lower()
+    print("")
+    if a=='what is rest':
+        print("")
+        print("when an object does not change it's position with time and with respect to it's surrounding is called rest")
+
+    elif a=='what is motion':
+        print("when an object changes it's position and does not remain stationary with respect to it's surrounding is called motion.")
+
+    elif a=='give example of rest':
+        print("e.g 1--a book placed on a table")
+        print("e.g.2--standing car")
+
+    elif a=='give example of motion':
+        print("e.g.1--running car")
+        print("e.g.2--movement of fan")
+
+    elif a=='how rest and motion are related terms':
+        print("An object may be in rest with respect to one person but it can be in motion with respect to some other person.Thus we can say that,")
+        print("          Rest and Motion are relative terms")
+        print("e.g.-- for a passenger in a moving bus,the road side passengers and trees appears to be moving but for a person standing on road side feels that bus is")
+        print("moving")
+
+    elif a== 'what is reference point':
+        print("the location of an object is given with respect to a reference point.")
+
+    elif a=='what is origin':
+        print("a fixed point with respect to which given object changes it's position is called origin")
+
+    elif a=='how many types of motion are there':
+        print("there are three main types of motion:-")
+        print("1.linear motion")
+        print("2.circular motion")
+        print("3.periodic motion")
+
+    elif a=='what is linear motion':
+        print("when an object moves along a straight line path then it is said to be linear motion. It is also called rectilinear motion.")
+        print("e.g.-car moving on a straight line path")
+        
+    elif a=='what is circular motion':
+        print("when an object moves around a fixed point then motion is said to be circular motion,It is also called rotatory motion.")
+        print("e.g.--blades of windmill")
+
+    elif a=='what is periodic motion':
+        print("when an object moves after a fixed interval of time then motion is said to be periodic.It is also called oscillatory motion.")
+        print("e.g.--motion of pendulum")
+
+    elif a=='what is scalar quantity':
+        print("those quantities which have only magnitude but no direction,those quantites are called scalar quantity.")
+        print("e.g.- speed")
+
+    elif a=='what is vector quantity':
+        print("those quantities which have both magnitude as well as direction,those quantities are called vector quantity.")
+        print("vector quantites are denoted by arrow(->)on the head of quantites")
+        print("e.g.--velocity")
+
+    elif a=='what is distance':
+        print("the total length of path covered by the object is called distance.")
+        print("It is a scalar quantity")
+        print("distance is always taken as positive.It is never negative or zero")
+        print("SI unit of speed= Metre(m)")
+        print("CGS unit of speed = centimetre(cm)")
+
+    elif a=='what is SI unit of distance':
+        print("metre(m)")
+
+    elif a=='what is CGS unit of distance':
+        print("centimetre")
+
+    elif a=='what is speedometre':
+        print("the device which meaures speed of vehicles is speedometre")
+
+    elif a== 'what is displacement':
+        print(" ")
+        print("the shortest distance covered by an object from initial to finl position is called displacement")
+        print("       ")
+        print ("             displacement=final position-initial position            ")
+        print(" ")
+        print("displacement can be positive(+),negative(-) or zero(0)")
+        print("     ")
+        print("displacement is a vector quantity")
+        print ("     ")
+        print("SI unit of displacement = metre(m)")
+        print("CGS unit of displacement = centimetre(cm)")
+
+    elif a=='how many types of displacement are there':
+        print("there are three types of displacement")
+        print("1.positive displacement")
+        print("2.negative displacement")
+        print("3.zero displacement")
+
+    elif a=='what is positive displacement':
+        print("when an object moves from initial to final position then it is said to be positive displacement")
+
+    elif a=='what is negative displacement':
+        print("when an object moves from initial to final position and returns towards the initial position then it s said to be negative displacement")
+
+    elif a=='what is zero displacement':
+        print("when an object moves from initial to final position and reached back at initial position then it is said to be zero displacement")
+
+    elif a== 'what is difference between distance and displacement':
+        print("1.Distance is the total length of path covered whereas displacement is the shortest distance from initial to final position")
+        print("2.Distance cannot be zero whereas displacement can be zero")
+        print("3.Distance is always positive for moving body whereas displacement can be positive,negative or zero")
+        print("4. Distance is a scalar quantity whereas displacement is a vector quantity")
+
+    elif a== 'what is uniform motion':
+        print("when an object covers equal distance in equal interval of time then it is said to be uniform motion")
+        print("                                                                                               ")
+        print("an object performing uniform motion,the total distance covered is directly proportional to total time taken")
+        print("e.g.--moving of fan because it complete it's one round in equal time")
+
+    elif a=='in which motion total distance covered is directly proportional to total time taken':
+        print("uniform motion")
+
+    elif a== 'what is non uniform motion':
+        print("when an object covers equal distance in unequal interval of time or unequal distance in equal interval of time")
+        print("then the motion is said to be non uniform motion")
+
+        print("e.g.--moving of vehicle on a crowded road")
+
+    elif a=='what is speed':
+        print("The distance covered in per unit time is called speed")
+        print("                  distance(m)         ")
+        print("            speed=-------------       ")
+        print("                    time(s)          ")
+        print("                                   ")
+        print("                    or             ")
+        
+        print("                     s            ")
+        print("                v = ---          ")
+        print("                     t          ")
+        print("SI unit of speed = metre/second or m/s")
+        print("                                   ")
+        print("speed is a scalar quantity.")
+        print("                   ")
+        print("Speed is always positive for moving body.")
+        print("                          ")
+        print("speed is of four types:")
+        print("1.Instantaneous speed:-the speed of an object at a particular moment of time during the motion of the object is called instantaneous motion")
+        print("                                                                                               ")
+        print("2.Uniform speed:-speed of an object is said to be uniform when it covers equal distance in equal interval of time ")
+        print("                                                                ")
+        print("3.Non uniform speed:- speed of an object is said to be non uniform when it covers unequal distance in equal interval of time or equal distance in")
+        print("                      equal interval of time")
+        print('                                          ')
+        print("4.Average speed :-The ratio of total distance travelled to total time taken by an object is said to be average speed")
+        print("                                   ")
+        print("                               Total distance                    ")
+        print("             average speed = ------------------                  ")
+        print("                             total time taken                    ")
+
+    elif a=='what is velocity':
+        print("The displacement covered by an object in per unit time is called velocity")
+        print("                     ") 
+        print("                    Displacement                      ")
+        print("     velocity = ---------------------                ")
+        print("                    Time                           ")
+        print("              ")
+        print(" Velocity is a vector quantity. ")
+        print("      ")
+        print("Velocity can be positive,negative or zero.")
+        print("         ")
+        print("   SI unit of velocity = metre/second   or m/s    ")
+        print("   CGS unit of velocity = centimetre/second    or  cm/s ")
+        print (" ")
+        print("velocity is of three types:-")
+        print(" ")
+        print("1. Uniform Velocity- When an object covers equal displacement in equal interval of time then velocity is said to be uniform velocity.")
+        print(" ")
+        print("2. Non-uniform Velocity-When an object covers equal displacemement in unequal interval of time or unequal displacement in equal interval of time ")
+        print("                        then velocity is said to be non uniform velocity")
+        print("     ")
+        print("3. Average Velocity:- The ratio of total displacement to the total time taken is called average velocity.")
+        print("     ")
+        print("                          total displacement      ")
+        print("     average velocity = ----------------------   ")
+        print("                          total time taken     ")
+        print("        ")
+        print("     ")
+        print("                        initial velocity + final velocity ")
+        print(" average velocity =  -------------------------------------  ")
+        print("                                        2                    ")
+
+    elif a=='what is acceleration':
+        print("The change in the velocity of an object in per unit time is called acceleration.")
+        print("")
+        print("                change in velocity ")
+        print("acceleration = ----------------------")
+        print("                   time taken         ")
+        print("")
+        print("Acceleration is a vector quantity")
+        print("")
+        print("SI unit of acceleration = metre/second square")
+        print("")
+        print(" Acceleration is of four types")
+        print("1. Uniform Acceleration--If an objecct's velocity increases or decreases by equal amount in equal interval of ")
+        print("                          time then acceleration is said to be uniform acceleration.")
+        print("")
+        print("2.Non Uniform Acceleration--If the velocity of an object changes by unequal amount in equal interval ")
+        print("                           of then acceleration is said to be non-uniform acceleration ")
+        print("")
+        print("3.Positive acceleration-- If the acceleration is in the direction of velocity then velocity is said ")
+        print("                           to be positive acceleration")
+        print("")
+        print(" 4. Negative Acceleration--i)If the acceleration is in the opposite direction of velocity then velocity ")
+        print("                             is said to be positive acceleration")
+        print("                           ii)Negative acceleration is also called retradation")
+        print("                           iii)In this case  final velocity<initial velocity")        
+    
+    else:
+        print("result not found")
+    
+    c=input("Do you want to repeat the program:yes/no").lower()
+    if c=="yes":
+        continue
+    else:
+        exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
